9917556798a387630cab487129529c80f2f19d12
Adem-elwafi <adem.elwafi.dev@gmail.com>
2025-10-14 23:49:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen bg-gray-100 text-gray-800">
+    <header class="bg-white shadow p-4">
+      <h1 class="text-xl font-semibold">💰 Budget Manager</h1>
+    </header>
+    <main class="p-4">
+      <router-view />
+    </main>
+  </div>
+</template>
